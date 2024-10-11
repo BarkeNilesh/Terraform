abe55a33bad0dff41aede663cae29a48b6775c6e
Nilesh Barke <154496524+BarkeNilesh@users.noreply.github.com>
2024-10-11 16:45:02 +0530

    Add files via upload

---
+++ 2-variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+    type = string
+    default = "Nilesh"
+  
+}
+variable "age" {
+    type = number
+    default = "25"
+  
+}
---
+++ 3-resource.tf
@@ -0,0 +1,4 @@
+output "Details" {
+    value = "Hello, ${var.name}. Your age is ${var.age}"
+  
+}
