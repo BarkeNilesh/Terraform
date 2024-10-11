b98295c9c884a31de3617d5ac51e13b25e41c369
BarkeNilesh <nilbarke28@gmail.com>
2024-10-11 17:32:27 +0530

    Added Funtion in resource

--- 3-resource.tf
+++ 3-resource.tf
@@ -6,3 +6,12 @@ output "FavSubject" {
     value = "My Fav Subject is ${var.books[2]}"
   
 }
+// This is function
+output "FirstSubject" {
+    value = "My 1st Subject is ${upper(var.books[0])}"
+  
+}
+output "secondSubject" {
+    value = "My 1st Subject is ${lower(var.books[1])}"
+  
+}
